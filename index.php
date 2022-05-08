51278dfab97e5f9e980fa5b2b1454b1c797b5674
Saphal Jha <safal.jha5@gmail.com>
2022-05-08 12:31:22 +0530

    added index file in project

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+	test test test
+
+</body>
+</html>
